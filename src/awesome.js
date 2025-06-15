8e258adab3fcc0a5229fc71e519b54a410a383cb
Priyanshu <tskpriyanshu2005@gmail.com>
2025-06-15 16:49:06 +0530

    new try

--- src/awesome.js
+++ src/awesome.js
@@ -2,6 +2,7 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
-    //thanks for this tutorial it is very helpful
+ alert('awesome learning');
+    console.log('learning git is fasinating');
   }
 }
